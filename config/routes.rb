5d8236f9179e3b124492aa5d87a5473153e0d78d
mayu_kishimoto <nhpw.29@gmail.com>
2023-04-17 16:37:52 +0900

    お気に入り機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  resources :blogs
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only:[:new, :create, :show, :edit]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :favorites, only: [:create, :destroy]
 end
